cea38a2f45c1a28d816609afdddeabd56141f7e3
csadetec <informaticact@santoagostinho.com.br>
2019-05-29 21:50:54 -0300

    pontos listar

--- application/views/pontos/pontos_listar.php
+++ application/views/pontos/pontos_listar.php
@@ -1,5 +1,20 @@
- <input class="form-control col-12 mt-3" type="search" placeholder="Pesquisar.." aria-label="Pesquisar.." id="myInput" data-list="list-group">
-<?php 
+<input class="form-control col-12 mt-3" type="search" placeholder="Pesquisar.." aria-label="Pesquisar.." id="myInput" data-list="list-group">
+
+<div class="card mt-3"> 
+  <?php if($turmas): ?>
+  <ul class="list-group list-group-flush">
+    <?php foreach($turmas as $row): ?>
+    <li class="list-group-item">
+      <?php echo '<h5>'.$row->nome_turma.'</h5>' ?>
+      Pontos:  <?php echo $row->pontos ?>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+  <?php endif; ?>
+
+</div>
+
+<?php
 
   if(isset($turmas)):
     $this->table->set_heading('Turma', 'Pontos', 'Editar');
